e7b01930dfd70d6a0d3b3e1906ff5428543e8f75
annuu1 <mannusng876@gmail.com>
2024-11-15 10:29:18 +0530

    completion of the calculator project

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -28,6 +28,12 @@ function clearDisplay() {
 function calculateResult() {
     const display = document.getElementById('display');
     try {
+        let result = eval(display.value);
+        if (condition) {
+            
+        } else {
+            
+        }
         display.value = eval(display.value);
     } catch (error) {
         display.value = 'Error';
@@ -35,4 +41,8 @@ function calculateResult() {
 }
 function isOperator(value) {
     return ['+', '-', '*', '/'].includes(value);
+}
+function removeLastDigit(){
+    let displayVal = document.getElementById('display').value ;
+    document.getElementById('display').value = displayVal.slice(0, -1);
 }
